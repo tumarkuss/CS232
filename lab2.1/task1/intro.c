311d22666fabd76d4f9c83ffb4ba3412a57ea1b1
Tumar Kussainov <37127858+tumarkuss@users.noreply.github.com>
2020-02-12 22:39:59 -0500

    Add files via upload
    tested

--- lab2.1/task1/intro.c
+++ lab2.1/task1/intro.c
@@ -3,14 +3,18 @@
 #include <stdio.h>
 #define PI 3.14159
 
-int main()
-{
-  float r, a;
+int main(){
+    float r, a,x, circmf;
 
-  printf("Enter radius (in inches):\n");
-  scanf("%f", &r);
+    printf("Enter radius (in mm):\n");
+    scanf("%f", &x);
+    r = x/25.4;
 
-  a = PI * r * r;
+    a = PI * r * r;
 
-  printf("Circle's area is %3.2f (sq in).\n", a);
+    circmf = 2 * PI * r;
+
+    printf("Circle's area is %3.2f (sq in).\n", a);
+
+    printf("Circumference is %3.2f (sq in).\n", circmf);
 }
